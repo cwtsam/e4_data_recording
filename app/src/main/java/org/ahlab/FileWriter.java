f2cd951844120a64856aa3955e35fa4b9c4d85f2
Samantha Chan <sam.chanWT@gmail.com>
2019-04-09 19:34:12 +1200

    added file writing java and change in manifest to allow file writing to phone

---
+++ app/src/main/java/org/ahlab/FileWriter.java
@@ -0,0 +1,120 @@
+package org.ahlab;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileWriter {
+	private static final String TAG = "[FileWriter]";
+	private static FileWriter instance;
+	private List<String> acceleration;
+	private List<String> ibi;
+	private String name;
+	private int gender;
+	private int session;
+	private String age;
+	private String basePath;
+
+	private boolean ready;
+
+	private FileWriter() {
+		acceleration = new ArrayList<>();
+		ibi = new ArrayList<>();
+	}
+
+	public static synchronized FileWriter getInstance() {
+		if (instance == null) {
+			instance = new FileWriter();
+		}
+		return instance;
+	}
+
+	public boolean initSession(String name, String age, int gender, int session) {
+		this.name = name;
+		this.age = age;
+		this.gender = gender;
+		this.session = session;
+
+		Log.i(TAG, "initSession: DocDir " + Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS).exists());
+		File baseDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "Prospero"); // can change the directory from here
+		if (!baseDir.exists()) {
+			Log.i(TAG, "initSession: creating base dir");
+			boolean status = baseDir.mkdir();
+			Log.i(TAG, "initSession: base dir creation: " + status);
+		}
+		File user = new File(baseDir, name);
+		if (!user.exists()) {
+			boolean status = user.mkdir();
+			Log.i(TAG, "user not exists, creating dir: " + status);
+		} else {
+			Log.i(TAG, "user exists: " + user.getAbsolutePath());
+		}
+		int i = 0;
+		String attemptIdx = String.valueOf(i);
+		File attempt = new File(user, attemptIdx);
+		while (attempt.exists()) {
+			attemptIdx = String.valueOf(++i);
+			attempt = new File(user, attemptIdx);
+			Log.i(TAG, "initFileStructure: session exist: " + attemptIdx);
+		}
+		attempt.mkdir();
+		basePath = attempt.getPath();
+
+		writeInfoFile();
+
+		ready = true;
+		return ready;
+	}
+
+	private void writeInfoFile() {
+		String path = basePath + "/info.txt";
+		StringBuilder stringBuilder = new StringBuilder();
+		stringBuilder.append("Name: ").append(name).append("\n");
+		stringBuilder.append("Age: ").append(age).append("\n");
+		stringBuilder.append("Gender: ").append(gender).append("\n");
+		stringBuilder.append("Session: ").append(session);
+		write(path, stringBuilder);
+	}
+
+	private void write(String path, StringBuilder stringBuilder) {
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new java.io.FileWriter(path, true))) {
+			bufferedWriter.append(stringBuilder);
+			bufferedWriter.flush();
+		} catch (IOException e) {
+			Log.e(TAG, "writeCSV: error while writing", e);
+		}
+	}
+
+	public synchronized void appendAcceleration(String record) {
+
+		acceleration.add(record);
+		if (acceleration.size() > 100) {
+			String path = basePath + "/accelerometer.csv";
+			writeFile(acceleration, path);
+			acceleration = new ArrayList<>();
+		}
+	}
+
+	private void writeFile(List<String> accel, String path) {
+		StringBuilder stringBuilder = new StringBuilder();
+		for (String record : accel) {
+			stringBuilder.append(record).append("\n");
+		}
+		write(path, stringBuilder);
+	}
+
+	public synchronized void appendIBI(String record) {
+
+		ibi.add(record);
+		if (ibi.size() > 10) {
+			String path = basePath + "/ibi.csv";
+			writeFile(ibi, path);
+			ibi = new ArrayList<>();
+		}
+	}
+}
